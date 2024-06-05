5097d974bd5bd22ea41ed012a07177b2c07cd5da
Your Name <eanerghes@gmail.com>
2024-06-05 14:38:13 +0300

    local deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Clone or update the source repository
-if [ ! -d "main" ]; then
-    git clone -b feature2 git@github.com:user/source_repo.git
-else
-    cd main
-    git fetch origin
-    git checkout feature2
-    git pull
-    cd ..
-fi
+# Define the source and destination directories
+SOURCE_DIR="C:\Users\a\source\repos\ImageProcessingApp\src"
+DESTINATION_DIR="C:\Users\a\source\repos\"
 
-# Navigate to the source directory
-cd main
+# Copy files from source to destination directory
+cp -r $SOURCE_DIR/* $DESTINATION_DIR/
 
-# Build or prepare your project (if needed)
-
-# Commit and push changes to the destination branch
-git add .
-git commit -m "Deploy changes"
-git push origin main
+# Optional: Print a message indicating the completion of the deployment
+echo "Deployment completed successfully!"
